dd5fed1e384c197cfa40dac60ea5a232eea46974
llslucas <lucas.souza473@outlook.com>
2026-04-19 20:54:32 -0300

    feat: add global styles file with color apallete

---
+++ src/styles/global.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    primary: "#8369f5",
+    background: "#ffffff",
+    text: "#1f1f1f",
+    border: "#696969ff",
+    white: "#ffffff",
+    black: "#000000",
+    focused: "#FFFFFF",
+    unfocused: "#B9B9B9",
+  },
+};
+
