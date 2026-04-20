c3b8087188506c11aad7b6cb69cc796f4ef75e64
llslucas <lucas.souza473@outlook.com>
2026-04-19 21:18:13 -0300

    feat: add color `textScondary` to global styles theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,7 @@ export const theme = {
     primary: "#8369f5",
     background: "#ffffff",
     text: "#1f1f1f",
+    textSecondary: "#8a8888",
     border: "#696969ff",
     white: "#ffffff",
     black: "#000000",
