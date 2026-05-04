acae5278297799494cb4db0b5231b7dac61c8c1a
llslucas <lucas.souza473@outlook.com>
2026-05-04 20:52:01 -0300

    feat: add interceptor to axios api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,23 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("TOKEN");
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export { api };
